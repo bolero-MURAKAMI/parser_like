9510fa547b2548f865f45657834dc31f5565b348
DtYaZsK <dtyazsk_windows7@yahoo.co.jp>
2014-04-16 23:33:20 +0900

    Create string_traits.hpp

---
+++ string_traits.hpp
@@ -0,0 +1,62 @@
+//DYZ parser generator
+//string_traits.hpp
+//(c)DtYaZsK 2014
+
+#pragma once
+
+#include<string>
+
+namespace dyz_parser
+{
+	
+	inline bool string_match(std::string::const_iterator beg, std::string::const_iterator end, const char* str)
+	{
+		while (true)
+		{
+			if (*str == '\0')
+			{
+				return true;
+			}
+			if (beg == end)
+			{
+				return false;
+			}
+			if (*beg != *str)
+			{
+				return false;
+			}
+			++beg;
+			++str;
+		}
+	}
+	inline size_t string_literal_check(std::string::const_iterator beg, std::string::const_iterator end)
+	{
+		if (*beg != '\"')
+			return 0;
+		bool is_escape = false;
+		auto ite = beg;
+		for (; ite != end; ++ite)
+		{
+			if (is_escape)
+			{
+				is_escape = false;
+				continue;
+			}
+			if (*ite == '\"')
+				return std::distance(beg, ite) + 1;
+			if (*ite == '\\')
+				is_escape = true;
+		}
+		return false;
+	}
+
+	inline bool token_string_check(char c)
+	{
+		return ((static_cast<int>('a') <= static_cast<int>(c)) &&
+			(static_cast<int>(c) <= static_cast<int>('z'))) ||
+			((static_cast<int>('A') <= static_cast<int>(c)) &&
+			(static_cast<int>(c) <= static_cast<int>('Z'))) ||
+			((static_cast<int>('0') <= static_cast<int>(c)) &&
+			(static_cast<int>(c) <= static_cast<int>('9')));
+	}
+}
