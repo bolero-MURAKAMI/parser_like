1004c39d08c95dd0034b9ff283ef36f37074751a
DtYaZsK <dtyazsk_windows7@yahoo.co.jp>
2014-04-18 18:35:30 +0900

    Update parser.hpp

--- parser.hpp
+++ parser.hpp
@@ -4,235 +4,94 @@
 
 #pragma once
 
-#include"sentence_data.hpp"
-#include"string_traits.hpp"
-#include<list>
 #include<string>
+#include<list>
+#include<type_traits>
 
 namespace dyz_parser
 {
-	struct parse_result
+	namespace parse_helper
 	{
-		std::list<std::string> tokens_;
-		std::list<std::list<std::string>> tuples_;
-		std::list<std::string> str_lites_;
-		std::list<sentence_data>::const_iterator ite_;
-		bool is_valid;
-		size_t type_num;
+		struct token_holder_type{};
+		struct tuple_holder_type{};
+		struct space_holder_type{};
+		struct str_literal_type{};
+
+		enum class data_type
+		{
+			token, string, tuple, str_literal, space
+		};
+
+		struct data
+		{
+			std::string str_;
+			data_type type_;
+			data(token_holder_type) :type_(data_type::token){}
+			data(tuple_holder_type) :type_(data_type::tuple){}
+			data(space_holder_type) :type_(data_type::space){}
+			data(str_literal_type) :type_(data_type::str_literal){}
+			data(std::string str) :str_(str), type_(data_type::string){}
+		};
+	}
+
+	namespace placeholder
+	{
+		const parse_helper::token_holder_type token_holder;
+		const parse_helper::tuple_holder_type tuple_holder;
+		const parse_helper::space_holder_type space_holder;
+		const parse_helper::str_literal_type string_literal_holder;
+	}
+
+	struct sentence_data
+	{
+		std::list<parse_helper::data> sentence;
+		template<class T, class... Ts>void init_impl(T arg, Ts... args)
+		{
+			init_impl(args...);
+			sentence.emplace_front(arg);
+		}
+		void init_impl()
+		{
+
+		}
+		template<class... Ts>void init(Ts... args)
+		{
+			init_impl(args...);
+			if (sizeof...(Ts) <= 1)
+				return;
+			auto ite = sentence.begin();
+			auto before = sentence.begin();
+			++ite;
+			auto end = sentence.end();
+			for (; ite != end; ++ite)
+			{
+				before = std::prev(ite);
+				if ((*before).type_ == parse_helper::data_type::token &&
+					(*ite).type_ == parse_helper::data_type::token)
+				{
+					sentence.emplace(ite, " ");
+					sentence.emplace(ite, placeholder::space_holder);
+				}
+				else if ((*before).type_ == parse_helper::data_type::token &&
+					(*ite).type_ == parse_helper::data_type::string&&
+					token_string_check(*(*ite).str_.c_str()))
+				{
+					sentence.emplace(ite, " ");
+					sentence.emplace(ite, placeholder::space_holder);
+				}
+				else if((*ite).type_ == parse_helper::data_type::token &&
+					(*before).type_ == parse_helper::data_type::string &&
+					token_string_check(*(std::prev((*before).str_.end()))))
+				{
+					sentence.emplace(ite, " ");
+					sentence.emplace(ite, placeholder::space_holder);
+				}
+			}
+		}
+
+		template<class... Ts>sentence_data(Ts... arg)
+		{
+			init(arg...);
+		}
 	};
-	
-	namespace detail
-	{
-		//parse type
-		inline size_t parse_type_case_token(std::string::const_iterator beg, std::string::const_iterator end)
-		{
-			int ret = 0;
-			for (; beg!=end && token_string_check(*beg); ++beg, ++ret);
-			return ret;
-		}
-
-		inline size_t parse_type_case_tuple(
-			std::string::const_iterator beg, 
-			std::string::const_iterator end)
-		{
-			if (*beg != '(')
-				return 0;
-			auto ite = beg;
-			for (int i = 1; i != 0;)
-			{
-				++ite;
-				if (ite == end)
-				{
-					return 0;
-				}
-				if (*ite == ')')
-				{
-					--i;
-				}
-				else if (*ite == '(')
-				{
-					++i;
-				}
-			}
-			++ite;
-			return std::distance(beg, ite);
-		}
-
-		inline size_t parse_type_case_space(
-			std::string::const_iterator beg, 
-			std::string::const_iterator end)
-		{
-			size_t ret = 0;
-			for (; *beg == ' '; ++beg, ++ret);
-			return ret;
-		}
-
-		inline bool parse_type_impl(
-			sentence_data const& data, 
-			std::string const& line)
-		{
-			auto ite = line.begin();
-			auto end = line.end();
-			for (auto const& d : data.sentence)
-			{
-				if (d.type_ == parse_helper::data_type::token)
-				{
-					if (ite == end)
-						return false;
-					size_t shift = parse_type_case_token(ite, end);
-					if (!shift)
-						return false;
-					ite = std::next(ite, shift);
-				}
-				else if (d.type_ == parse_helper::data_type::string)
-				{
-					if (ite == end)
-						return false;
-					if (!string_match(ite, end, d.str_.c_str()))
-						return false;
-					ite = std::next(ite, d.str_.size());
-				}
-				else if (d.type_ == parse_helper::data_type::tuple)
-				{
-					if (ite == end)
-						return false;
-					size_t shift = parse_type_case_tuple(ite, end);
-					if (!shift)
-						return false;
-					ite = std::next(ite, shift);
-				}
-				else if (d.type_ == parse_helper::data_type::str_literal)
-				{
-					auto shift = string_literal_check(ite, end);
-					if (!shift)
-						return false;
-					ite = std::next(ite, shift);
-				}
-				else
-				{
-					while (true)
-					{
-						if (ite == end)
-							break;
-						if (*ite != ' ')
-							break;
-						++ite;
-					}
-				}
-			}
-			return ite == end;
-		}
-
-		inline auto parse_type(std::list<sentence_data> const& data, std::string const& line)
-			->std::pair<int,decltype(data.begin())>
-		{
-			int i = 0;
-			for (auto d = data.begin(); d != data.end(); ++d)
-			{
-				if (parse_type_impl(*d, line))
-					return std::make_pair(true, d);
-				++i;
-			}
-			return std::make_pair(-1,data.begin());
-		}
-
-		//parse
-		inline std::pair<size_t,std::string> read_token(std::string::const_iterator beg, std::string::const_iterator end)
-		{
-			auto ite = beg;
-			for (; ite != end && token_string_check(*ite); ++ite);
-			return std::make_pair(std::distance(beg,ite), std::string(beg, ite));
-		}
-
-		inline std::pair<size_t, std::list<std::string>>
-				read_tuple(std::string::const_iterator beg,std::string::const_iterator end)
-		{
-			auto ite = beg;
-			auto sta = std::next(beg);
-			std::list<std::string> ret;
-			for (int i = 1; i != 0;)
-			{
-				++ite;
-				if (ite == end)
-				{
-					return std::make_pair(std::distance(beg, ite), ret);
-				}
-				if (*ite == '(')
-				{
-					++i;
-				}
-				else if (*ite == ')')
-				{
-					--i;
-				}
-				else if (i == 1 && *ite == ',')
-				{
-					ret.emplace_back(sta, ite);
-					sta = std::next(ite);
-				}
-			}
-			ret.emplace_back(sta, ite);
-			++ite;
-			return std::make_pair(std::distance(beg, ite), ret);
-		}
-
-		inline std::pair<size_t, std::string> read_str_lite(std::string::const_iterator beg,std::string::const_iterator end)
-		{
-			auto shift = string_literal_check(beg, end);
-			auto e = std::next(beg, shift - 1);
-			return std::make_pair(shift, std::string(std::next(beg, 1), e));
-		}
-
-		inline parse_result parse(sentence_data const& data, std::string const& line)
-		{
-			auto beg = line.begin();
-			auto end = line.end();
-			parse_result ret;
-			ret.is_valid = true;
-			for (const auto& d : data.sentence)
-			{
-				if (d.type_ == parse_helper::data_type::token)
-				{
-					auto value = read_token(beg, end);
-					ret.tokens_.push_back(std::get<1>(value));
-					beg = std::next(beg, std::get<0>(value));
-				}
-				else if (d.type_ == parse_helper::data_type::string)
-				{
-					beg = std::next(beg, d.str_.size());
-				}
-				else if (d.type_ == parse_helper::data_type::tuple)
-				{
-					auto value = read_tuple(beg, end);
-					ret.tuples_.push_back(std::get<1>(value));
-					beg = std::next(beg, std::get<0>(value));
-				}
-				else if (d.type_ == parse_helper::data_type::str_literal)
-				{
-					auto value = read_str_lite(beg, end);
-					ret.str_lites_.push_back(std::get<1>(value));
-					beg = std::next(beg, std::get<0>(value));
-				}
-				else
-				{
-					for (; *beg == ' '; ++beg);
-				}
-			}
-			return ret;
-		}
-	}
-
-	inline parse_result parse_line(std::list<sentence_data> const& data, std::string line)
-	{
-		auto type = detail::parse_type(data, line);
-		if (std::get<0>(type)==-1)
-		{
-			parse_result ret;
-			ret.is_valid = false;
-			return ret;
-		}
-		auto ret= detail::parse(*std::get<1>(type), line);
-		ret.type_num = std::get<0>(type);
-		return ret;
-	}
 }
